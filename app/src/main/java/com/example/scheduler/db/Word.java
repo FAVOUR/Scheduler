10926049c3593f3688bdea66313d7de5a0d8b5d2
FAVOUR <truelyfavoured@gmail.com>
2019-08-15 08:21:29 +0100

    Created the word table by adding the entity class

---
+++ app/src/main/java/com/example/scheduler/db/Word.java
@@ -0,0 +1,44 @@
+package com.example.scheduler.db;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+/**
+ * Created by Olije Favour on 8/15/2019.
+ * Copyright (c) 2019  Itex Integrated Services  All rights reserved.
+ */
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    //You can either setup the Entity like so or.....
+//  @PrimaryKey
+//    @NonNull
+//    public  int id;
+//    public   String word;
+
+
+//    like so....
+      @PrimaryKey
+      @NonNull
+     private int id;
+      @ColumnInfo(name = "word")
+     private  String mWord;
+
+    public Word(int id ,String word){
+
+        this.id=id;
+        mWord=word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+
+}
---
+++ app/src/main/java/com/example/scheduler/schedule/Activity/ScheduleActivity.java
@@ -0,0 +1,16 @@
+package com.example.scheduler.schedule.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.scheduler.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
