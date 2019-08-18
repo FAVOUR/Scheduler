f99d5b3b4a952af58a7a2260444d7c6d4e1132e4
FAVOUR <truelyfavoured@gmail.com>
2019-08-18 18:59:43 +0100

    Added the repository -single source for data source

---
+++ app/src/main/java/com/example/scheduler/model/repository/WordRepositoty.java
@@ -0,0 +1,58 @@
+package com.example.scheduler.model.repository;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.scheduler.db.Word;
+import com.example.scheduler.db.WordDB;
+import com.example.scheduler.db.WordDao;
+
+import java.util.List;
+
+
+/**
+ * Created by Olije Favour on 8/18/2019.
+ * Copyright (c) 2019.
+ */
+
+
+public class WordRepositoty {
+  private  WordDao wordDoa;
+  private LiveData<List<Word>> allNotes;
+
+    WordRepositoty(Application application){
+
+        WordDB wordDB=  WordDB.getDatabase(application);
+        wordDoa = wordDB.getWordDoa();
+        allNotes = wordDoa.getAllWord();
+    }
+
+    public void insert(Word word ){
+        new WordAsyncTask(wordDoa).doInBackground(word);
+    }
+
+
+    public LiveData<List<Word>> getAllNotes() {
+        return allNotes;
+    }
+
+    class WordAsyncTask extends AsyncTask<Word,Void,Void>{
+
+        WordDao wordDao;
+
+        public WordAsyncTask(WordDao wordDao) {
+            this.wordDao = wordDao;
+        }
+
+        @Override
+        protected Void doInBackground(Word... words) {
+
+             wordDao.insert(words[0]);
+
+            return null;
+        }
+    }
+
+}
